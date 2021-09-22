859d49e84355278e4dcf82d91aadcf38e56cc55a
prasadv1998 <prasadan1720@gmail.com>
2021-09-22 14:59:30 +0530

    Added: Addition

---
+++ src/main/java/com/javapractice/Addition.java
@@ -0,0 +1,11 @@
+package com.javapractice;
+
+public class Addition {
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 15;
+		int c;
+		c = a + b;
+		System.out.println("Total:" +c);
+	}
+}
